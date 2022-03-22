2ce86f64963ef6d391070fcd00b1806f162260b0
hades24 <94883043+hades24@users.noreply.github.com>
2022-03-22 04:29:26 +0000

    p1

---
+++ p1final.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+ int input()
+{
+  int n;
+  printf("enter sides of triangle \n");
+  scanf("%d", &n);
+  return n;
+}
+int sides(int a, int b, int c){
+  if(a==b || b==c || a==c){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+void output(int value)
+{
+  if(value==1){
+    printf("given triangle is scalene");
+  }
+  else{
+    printf("given triangle is not scanlene");
+  }
+}
+int main()
+{
+  int a=input();
+  int b=input();
+  int c=input();
+  int value=sides(a,b,c);
+  output(value);
+  return 0;
+}
