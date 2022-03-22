dad5f9742f31700fc264999707d1d71166a0c649
hades24 <94883043+hades24@users.noreply.github.com>
2022-03-22 04:20:00 +0000

    p0

--- p0final.c
+++ p0final.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+#include <math.h>
+ int input(){
+   int n;
+   printf("enter the base and height for triange \n");
+   scanf("%d", &n);
+   return n;
+ }
+int area(int a, int b){
+  float value=a*b/2;
+  return value;
+}
+void output(float value)
+{
+  printf("area of the triangle is %f",value);
+}
+int main()
+{
+  int a=input();
+  int b=input();
+  float value=area(a,b);
+  output(value);
+  return 0;
+}
